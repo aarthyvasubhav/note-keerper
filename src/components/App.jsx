e0fae57263286f90cbdc6c3eb0b799b7b0128eeb
Aarthy Adhibagavan <aarthyvasubhav003@gmail.com>
2023-08-30 05:40:19 -0700

    v2- added notes array and used arrowfunction with mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App(){
 
     return(
         <div>
         <Header />
-
-        <Note />
+        {notes.map(data => <Note key = {data.key} title = {data.title} content = {data.content}/>)}
         <Footer />
         </div>
     );
